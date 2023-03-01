da857e98d4fdb4e21c09890d4cd8db124afd7c57
dalilasampaio <dalilasampaio6@gmail.com>
2023-03-01 18:22:22 -0300

    Form New Project

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,14 @@
+import ProjectForm from '../projects/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject(){
-    return <h1>NewProject</h1>
+    return (
+     <div className={styles.newproject_container}>
+        <h1>Criar projeto</h1>
+        <p>Crie seus projetos para depois adicionar os serviços</p>
+        <ProjectForm btnText="Criar Projeto"/>
+    </div>
+    )
  }
  
  export default NewProject
