27bc5df96c322a06d28592e6671b9dc0abc464e9
dalilasampaio <dalilasampaio6@gmail.com>
2023-04-16 23:56:26 -0300

    removing projects

--- src/components/projects/ProjectCards.js
+++ src/components/projects/ProjectCards.js
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 import {BsPencil, BsFillTrashFill} from 'react-icons/bs'
 
 function ProjectCards({id,name, budget, category, handleRemove}){
+    const remove = (e) => {
+        e.preventDefault()
+        handleRemove(id)
+    }
+
     return(
         <div className={styles.project_card}>
             <h4>{name}</h4>
@@ -15,8 +20,8 @@ function ProjectCards({id,name, budget, category, handleRemove}){
             <div className={styles.project_card_actions}>
                 <Link to="/">
                     <BsPencil /> Editar
-                    </Link>
-                <button>
+                </Link>
+                <button onClick={remove}>
                     <BsFillTrashFill /> Excluir
                 </button>
             </div>
